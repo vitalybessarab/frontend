60e9c3c862f70a5fdef6037b470839aae044d9e0
Vitalii Bessarab <vitaly.bessarab@gmail.com>
2022-10-08 19:55:08 +0300

    add getting data from backend

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private baseUrl = 'http://localhost:8000';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getModulesCount(): Observable<any> {
+    return this.http.get(this.baseUrl.concat('/modules-count'));
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+
+  public modulesCount$: Observable<any>;
+
+  constructor(private api: ApiService) {
+    this.modulesCount$ = this.api.getModulesCount();
+  }
 }
